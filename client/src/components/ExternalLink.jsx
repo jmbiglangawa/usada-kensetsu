d93f7e6c192ceb63417e600c9884d4c9edc98b22
jmba909 <36124096+jmba909@users.noreply.github.com>
2021-01-24 15:12:27 +0800

    Bugfix for small Subscribe button

--- client/src/components/ExternalLink.jsx
+++ client/src/components/ExternalLink.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import {FiExternalLink} from 'react-icons/fi'
+import classNames from 'classnames'
 import '../css/ExternalLink.css'
 
-const ExternalLink = ({children, href, excludeIcon, style}) => {
+const ExternalLink = ({children, href, excludeIcon, style, className}) => {
     
     return(
-        <a className="external-link" href={href} target="_blank" rel="noopener noreferrer" style={style}>
+        <a className={classNames("external-link", className ? className : "")} href={href} target="_blank" rel="noopener noreferrer" style={style}>
             {children}
             {!excludeIcon && <FiExternalLink className="link-icon" />}
         </a>
